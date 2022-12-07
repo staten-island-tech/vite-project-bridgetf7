44885cb2a18dee36fafd04ce524d2a7685bd8750
bridgetf7 <fbridget0404@gmail.com>
2022-12-06 20:01:30 -0500

    commit

--- vite-project/js/cars.js
+++ vite-project/js/cars.js
@@ -2,33 +2,39 @@ const carsData = [
     {
         name: "Ford F-150 Raptor",
         type: "Truck",
-        price: 76775, 
-        imgUrl: "https://live.staticflickr.com/3687/10948774973_db516d692a_b.jpg"
-    },
-    {
-        name: "Audi Q7",
-        type: "SUV",
-        price: 57500, 
-        imgUrl: "https://cfx-wp-images.imgix.net/2022/10/2022-Audi-Q7-lede-scaled.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&s=fa1178371d8a5621e203c01b5c478c7e"
+        price: "$57,500",
+        imgURL: "https://cfx-wp-images.imgix.net/2022/10/2022-Audi-Q7-lede-scaled.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&s=fa1178371d8a5621e203c01b5c478c7e"
     },
     {
         name: "Mercedes Mayback GLS 600",
         type: "SUV",
-        price: 165100, 
-        imgUrl: "https://cfx-wp-images.imgix.net/2021/10/2020-11-07_Image_2021-Mercedes-GLS-600-Maybach-Green-002-scaled.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&s=0d852024d77d18aad10cb9e564410255"
+        price: "$165,100",
+        imgURL: "https://cfx-wp-images.imgix.net/2021/10/2020-11-07_Image_2021-Mercedes-GLS-600-Maybach-Green-002-scaled.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&s=0d852024d77d18aad10cb9e564410255"
     },
     {
         name: "Ford F-150 Lightning",
         type: "Electrified, Truck",
-        price: 54975,
-        imgUrl: "https://www.gannett-cdn.com/presto/2022/08/09/PDTF/3e2e3396-b772-4d46-a3ac-27f506065d7e-2023_Lightning.jpg"
+        price: "$54,975",
+        imgURL: "https://www.gannett-cdn.com/presto/2022/08/09/PDTF/3e2e3396-b772-4d46-a3ac-27f506065d7e-2023_Lightning.jpg"
     },
     {
         name: "Aston Martin DBX",
         type: "SUV",
-        price: 190585,
-        imgUrl: "https://mysterio.yahoo.com/mysterio/api/43066B3841097D28AF053CAE2635FA20D8CA267CAD0CC42157F8556192732C2A/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USD10ANS011A01300.jpg"
+        price: "$190,585",
+        imgURL: "https://mysterio.yahoo.com/mysterio/api/43066B3841097D28AF053CAE2635FA20D8CA267CAD0CC42157F8556192732C2A/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USD10ANS011A01300.jpg"
+    },
+    {
+        name: "Cadillac LYRIQ",
+        type: "Electrified, SUV",
+        price: "$69,795",
+        imgURL: "https://th.bing.com/th/id/R.6624d7ed458cdf0d8d1ce05a266d447d?rik=uRZ2Z76%2b%2bfQGlQ&pid=ImgRaw&r=0"
+    },
+    {
+        name: "Porsche 911 Carrera Cabriolet",
+        type: "Coup",
+        price: "$114,000",
+        imgURL: "https://th.bing.com/th/id/R.7c3bd2d52183fb873371b03b0e5c21f2?rik=mi%2f7br%2fS%2fxb2Tw&riu=http%3a%2f%2fcdn.carbuzz.com%2fgallery-images%2f1600%2f529000%2f300%2f529355.jpg&ehk=glS%2bz9t0nogMAv1XnEWv7fWeRDOTSaSjTk758KarnME%3d&risl=&pid=ImgRaw&r=0"
     }
 ];
 
-export {carsData};
+export { carsData };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,16 +1,21 @@
 import "/styles/style.css";
-import {carsData} from "./cars.js";
-console.log (carsData);
+import { carsData } from "./cars.js";
+console.log(carsData);
+
 
 carsData.forEach(carsData => {
     document.getElementById("products").insertAdjacentHTML
-    ("beforeend",
-      `
+        ("beforeend",
+            `
       <div id="productCard">
       <h2>${carsData.name}</h2>
       <p><img src=${carsData.imgURL}></p>
-      <p>${carsData.type}</p>
-      <p>${carsData.price}</p>
+      <p>Type: ${carsData.type}</p>
+      <p>Price: ${carsData.price}</p>
     `
-    )
-  });
+        )
+});
+
+
+
+
