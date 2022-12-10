64b4bc43abe3d08f4e09598e7aa00a6d026d81bc
bridgetf7 <fbridget0404@gmail.com>
2022-12-09 22:59:34 -0500

    commit

--- vite-project/js/cars.js
+++ vite-project/js/cars.js
@@ -1,23 +1,5 @@
 const carsData = [
     {
-        name: "Ford F-150 Raptor",
-        type: "Truck",
-        price: "$57,500",
-        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468044/ford_raptor_tdzzm4.jpg"
-    },
-    {
-        name: "Mercedes Mayback GLS 600",
-        type: "SUV",
-        price: "$165,100",
-        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468045/gls600_i76nom.jpg"
-    },
-    {
-        name: "Ford F-150 Lightning",
-        type: "Electric, Truck",
-        price: "$54,975",
-        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468045/lightning_bak5cn.jpg"
-    },
-    {
         name: "Aston Martin DBX",
         type: "SUV",
         price: "$190,585",
@@ -30,17 +12,36 @@ const carsData = [
         imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468044/lyriq_tr9tlx.jpg"
     },
     {
-        name: "Porsche 911 Carrera Cabriolet",
-        type: "Coupe",
-        price: "$114,000",
-        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468044/porsche911_icajfa.jpg"
-    },
-    {
         name: "Electric 1967 Ford Mustang",
         type: "Coupe, Electric",
         price: "$450,000",
         imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468044/evmustang_w31kjp.jpg"
+    }, {
+        name: "Ford F-150 Lightning",
+        type: "Electric, Truck",
+        price: "$54,975",
+        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468045/lightning_bak5cn.jpg"
+    },
+    {
+        name: "Ford F-150 Raptor",
+        type: "Truck",
+        price: "$57,500",
+        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468044/ford_raptor_tdzzm4.jpg"
+    },
+    {
+        name: "Mercedes Mayback GLS 600",
+        type: "SUV",
+        price: "$165,100",
+        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468045/gls600_i76nom.jpg"
+    },
+    {
+        name: "Porsche 911 Carrera Cabriolet",
+        type: "Coupe",
+        price: "$114,000",
+        imgURL: "https://res.cloudinary.com/dx8b7nhay/image/upload/v1670468044/porsche911_icajfa.jpg"
     }
+
 ];
+
 
 export { carsData };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,23 +2,26 @@ import "/styles/style.css";
 import { carsData } from "./cars.js";
 console.log(carsData);
 
-
-carsData.forEach(carsData => {
-    document.getElementById("products").insertAdjacentHTML
-        ("beforeend",
-            `
-      <div id="productCard">
-      <p><img src=${carsData.imgURL}></p>
-      <h2>${carsData.name}</h2>
-
-      <p id="outputText">Type: ${carsData.type}</p>
-      <p id="outputText">Price: ${carsData.price}</p>
-      <button id="shopBtn">Shop Now</button>
-      </div>
-    `
-        )
-});
-
+/*
+function createProduct() {
+    carsData.forEach(carsData => {
+        document.getElementById("products").insertAdjacentHTML
+            ("beforeend",
+                `
+          <div id="productCard">
+          <p><img src=${carsData.imgURL}></p>
+          <h2>${carsData.name}</h2>
+    
+          <p id="outputText">Type: ${carsData.type}</p>
+          <p id="outputText">Price: ${carsData.price}</p>
+          <button id="shopBtn">Shop Now</button>
+          </div>
+        `
+            )
+    });
+};
+//createProduct();
+*/
 
 //theme buttons
 document.getElementById("toggleTheme").addEventListener("click", function () {
@@ -31,50 +34,122 @@ document.getElementById("toggleTheme").addEventListener("click", function () {
     }
 });
 
-/*filters out the type of car
-carsData is my ARRAY, car is my chosen ITEM NAME
-*/
+//filter through selected car types
+const filterProducts = {
+    typeAll: function () {
+        document.getElementById("allCars").addEventListener("click", function () {
+            carsData
+                .forEach((car) => {
+                    console.log(car.name)
 
-//ALL 
-document.getElementById("allCars").addEventListener("click", function () {
-    carsData
-        .forEach((car) => {
-            console.log(car.name)
+                    document.getElementById("products").insertAdjacentHTML
+                        ("beforeend",
+                            `<div id="productCard">
+                        <p><img src=${car.imgURL}></p>
+                        <h2>${car.name}</h2>
+                        <p id="outputText">Type: ${car.type}</p>
+                        <p id="outputText">Price: ${car.price}</p>
+                        <button id="shopBtn">Shop Now</button>
+                        </div>
+                         `
+                        )
+                });
         });
-});
+    },
 
-//SUVs
-document.getElementById("SUVs").addEventListener("click", function () {
-    carsData
-        .filter((car) => car.type === "SUV")
-        .forEach((car) => {
-            console.log(car.name)
+    typeSUVs: function () {
+        document.getElementById("SUVs").addEventListener("click", function () {
+            carsData
+                .filter((car) => car.type === "SUV")
+                .forEach((car) => {
+                    console.log(car.name)
+                    document.getElementById("products").insertAdjacentHTML
+                        ("beforeend",
+                            `<div id="productCard">
+                            <p><img src=${car.imgURL}></p>
+                            <h2>${car.name}</h2>
+                            <p id="outputText">Type: ${car.type}</p>
+                            <p id="outputText">Price: ${car.price}</p>
+                            <button id="shopBtn">Shop Now</button>
+                            </div>
+                             `
+                        )
+                });
         });
-});
+    },
 
-//Electric
-document.getElementById("electric").addEventListener("click", function () {
-    carsData
-        .filter((car) => car.type === "electric")
-        .forEach((car) => {
-            console.log(car.name)
+    typeElectric: function () {
+        document.getElementById("electric").addEventListener("click", function () {
+            carsData
+                .filter((car) => car.type === "electric")
+                .forEach((car) => {
+                    console.log(car.name)
+                    document.getElementById("products").insertAdjacentHTML
+                        ("beforeend",
+                            `<div id="productCard">
+                        <p><img src=${car.imgURL}></p>
+                        <h2>${car.name}</h2>
+                        <p id="outputText">Type: ${car.type}</p>
+                        <p id="outputText">Price: ${car.price}</p>
+                        <button id="shopBtn">Shop Now</button>
+                        </div>
+                         `
+                        )
+                });
         });
-});
 
-//Trucks 
-document.getElementById("trucks").addEventListener("click", function () {
-    carsData
-        .filter((car) => car.type === "Truck")
-        .forEach((car) => {
-            console.log(car.name)
-        });
-});
+    },
 
-//Sedans & Coupes
-document.getElementById("sedansCoupes").addEventListener("click", function () {
-    carsData
-        .filter((car) => car.type === "Sedan", "Coupe")
-        .forEach((car) => {
-            console.log(car.name)
+    typeTrucks: function () {
+        document.getElementById("trucks").addEventListener("click", function () {
+            carsData
+                .filter((car) => car.type === "Truck")
+                .forEach((car) => {
+                    console.log(car.name)
+                    document.getElementById("products").insertAdjacentHTML
+                        ("beforeend",
+                            `<div id="productCard">
+                        <p><img src=${car.imgURL}></p>
+                        <h2>${car.name}</h2>
+                        <p id="outputText">Type: ${car.type}</p>
+                        <p id="outputText">Price: ${car.price}</p>
+                        <button id="shopBtn">Shop Now</button>
+                        </div>
+                         `
+                        )
+                });
         });
-});
+    },
+
+    typeSedansCoupes: function () {
+        document.getElementById("sedansCoupes").addEventListener("click", function () {
+            carsData
+                .filter((car) => car.type === "Sedan", "Coupe")
+                .forEach((car) => {
+                    console.log(car.name)
+                    document.getElementById("products").insertAdjacentHTML
+                        ("beforeend",
+                            `<div id="productCard">
+                        <p><img src=${car.imgURL}></p>
+                        <h2>${car.name}</h2>
+                        <p id="outputText">Type: ${car.type}</p>
+                        <p id="outputText">Price: ${car.price}</p>
+                        <button id="shopBtn">Shop Now</button>
+                        </div>
+                         `
+                        )
+                });
+        });
+    }
+
+};
+
+filterProducts.typeAll();
+filterProducts.typeElectric();
+filterProducts.typeSUVs();
+filterProducts.typeTrucks();
+filterProducts.typeSedansCoupes();
+
+
+
+
