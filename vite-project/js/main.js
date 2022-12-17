14845c9ea4ef0cd42b31d93860cd86b8732b02f8
bridgetf7 <fbridget0404@gmail.com>
2022-12-17 00:07:57 -0500

    commit

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,12 +2,11 @@ import "/styles/style.css";
 import { carsData } from "./cars.js";
 console.log(carsData);
 
-/*
-function createProduct() {
-    carsData.forEach(carsData => {
-        document.getElementById("products").insertAdjacentHTML
-            ("beforeend",
-                `
+
+const createProduct = function (carsData) {
+    document.getElementById("products").insertAdjacentHTML
+        ("beforeend",
+            `
           <div id="productCard">
           <p><img src=${carsData.imgURL}></p>
           <h2>${carsData.name}</h2>
@@ -17,11 +16,104 @@ function createProduct() {
           <button id="shopBtn">Shop Now</button>
           </div>
         `
-            )
-    });
-};
-//createProduct();
-*/
+        );
+}
+carsData
+    .forEach((car) => createProduct(car));
+
+
+const filterProducts = {
+    typeAll: function () {
+        document.getElementById("allCars").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .forEach((car) => {
+                    console.log(car.name)
+                    createProduct(car);
+                });
+        });
+    },
+    typeSUVs: function () {
+        document.getElementById("SUVs").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .filter((car) => car.type.includes("SUV"))
+                .forEach((car) => {
+                    console.log(car.name);
+                    createProduct(car);
+                });
+        });
+    },
+    typeElectric: function () {
+        document.getElementById("electric").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .filter((car) => car.type.includes("Electric"))
+                .forEach((car) => {
+                    console.log(car.name);
+                    createProduct(car);
+                });
+        });
+    },
+    typeTrucks: function () {
+        document.getElementById("trucks").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .filter((car) => car.type.includes("Truck"))
+                .forEach((car) => {
+                    console.log(car.name)
+                    createProduct(car);
+                });
+        });
+    },
+    typeCoupes: function () {
+        document.getElementById("coupes").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .filter((car) => car.type.includes("Coupe"))
+                .forEach((car) => {
+                    console.log(car.name)
+                    createProduct(car);
+                });
+        });
+    },
+    typeLowHigh: function () {
+        document.getElementById("priceLowHigh").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .sort((a, b) => a.price - b.price)
+                .forEach((car) => {
+                    console.log(car.name, car.price);
+                    createProduct(car);
+                });
+
+        });
+    },
+    typeHighLow: function () {
+        document.getElementById("priceHighLow").addEventListener("click", function () {
+            //document.querySelectorAll(".productCard").remove();
+            carsData
+                .sort((a, b) => b.price - a.price)
+                .forEach((car) => {
+                    console.log(car.name, car.price);
+                    createProduct(car);
+                });
+
+        });
+    }
+}
+
+filterProducts.typeAll();
+filterProducts.typeElectric();
+filterProducts.typeSUVs();
+filterProducts.typeTrucks();
+filterProducts.typeCoupes();
+filterProducts.typeLowHigh();
+filterProducts.typeHighLow();
+
+
+
+
 
 
 //theme buttons
@@ -36,168 +128,9 @@ document.getElementById("toggleTheme").addEventListener("click", function () {
     }
 });
 
-//filter through
-const filterProducts = {
-    typeAll: function () {
-        document.getElementById("allCars").addEventListener("click", function () {
-            carsData
-                .forEach((car) => {
-                    console.log(car.name)
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                        <p><img src=${car.imgURL}></p>
-                        <h2>${car.name}</h2>
-                        <p id="outputText">Type: ${car.type}</p>
-                        <p id="outputText">Price: ${car.price}</p>
-                        <button id="shopBtn">Shop Now</button>
-                        </div>
-                         `
-                        )
-                });
-        });
-    },
 
-    typeSUVs: function () {
-        document.getElementById("SUVs").addEventListener("click", function () {
-            carsData
-                .filter((car) => car.type.includes("SUV"))
-                .forEach((car) => {
-                    console.log(car.name)
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                            <p><img src=${car.imgURL}></p>
-                            <h2>${car.name}</h2>
-                            <p id="outputText">Type: ${car.type}</p>
-                            <p id="outputText">Price: ${car.price}</p>
-                            <button id="shopBtn">Shop Now</button>
-                            </div>
-                             `
-                        )
-                });
-        });
-    },
 
-    typeElectric: function () {
-        document.getElementById("electric").addEventListener("click", function () {
-            carsData
-                .filter((car) => car.type.includes("Electric"))
-                .forEach((car) => {
-                    console.log(car.name)
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                        <p><img src=${car.imgURL}></p>
-                        <h2>${car.name}</h2>
-                        <p id="outputText">Type: ${car.type}</p>
-                        <p id="outputText">Price: ${car.price}</p>
-                        <button id="shopBtn">Shop Now</button>
-                        </div>
-                         `
-                        )
-                });
-        });
 
-    },
-
-    typeTrucks: function () {
-        document.getElementById("trucks").addEventListener("click", function () {
-            carsData
-                .filter((car) => car.type.includes("Truck"))
-                .forEach((car) => {
-                    console.log(car.name)
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                        <p><img src=${car.imgURL}></p>
-                        <h2>${car.name}</h2>
-                        <p id="outputText">Type: ${car.type}</p>
-                        <p id="outputText">Price: ${car.price}</p>
-                        <button id="shopBtn">Shop Now</button>
-                        </div>
-                         `
-                        )
-                });
-        });
-    },
-
-    typeCoupes: function () {
-        document.getElementById("coupes").addEventListener("click", function () {
-            carsData
-                .filter((car) => car.type.includes("Coupe"))
-                .forEach((car) => {
-                    console.log(car.name)
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                        <p><img src=${car.imgURL}></p>
-                        <h2>${car.name}</h2>
-                        <p id="outputText">Type: ${car.type}</p>
-                        <p id="outputText">Price: ${car.price}</p>
-                        <button id="shopBtn">Shop Now</button>
-                        </div>
-                         `
-                        )
-                });
-        });
-    },
-
-    typeLowHigh: function () {
-        document.getElementById("priceLowHigh").addEventListener("click", function () {
-            carsData
-                .sort((a, b) => a.price - b.price)
-                .forEach((car) => {
-                    console.log(car.name, car.price);
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                    <p><img src=${car.imgURL}></p>
-                    <h2>${car.name}</h2>
-                    <p id="outputText">Type: ${car.type}</p>
-                    <p id="outputText">Price: ${car.price}</p>
-                    <button id="shopBtn">Shop Now</button>
-                    </div>
-                     `
-                        )
-
-                });
-
-        });
-    },
-
-    typeHighLow: function () {
-        document.getElementById("priceHighLow").addEventListener("click", function () {
-            carsData
-                .sort((a, b) => b.price - a.price)
-                .forEach((car) => {
-                    console.log(car.name, car.price);
-                    document.getElementById("products").insertAdjacentHTML
-                        ("beforeend",
-                            `<div id="productCard">
-                    <p><img src=${car.imgURL}></p>
-                    <h2>${car.name}</h2>
-                    <p id="outputText">Type: ${car.type}</p>
-                    <p id="outputText">Price: ${car.price}</p>
-                    <button id="shopBtn">Shop Now</button>
-                    </div>
-                     `
-                        )
-
-                });
-
-        });
-    }
-
-};
-
-filterProducts.typeAll();
-filterProducts.typeElectric();
-filterProducts.typeSUVs();
-filterProducts.typeTrucks();
-filterProducts.typeCoupes();
-filterProducts.typeLowHigh();
-filterProducts.typeHighLow();
 
 
 
